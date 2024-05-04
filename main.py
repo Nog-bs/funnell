07dc00ef6fa8c6196ccdd867c094b8ae31f27d74
Edro Gonzales <edrogonzales@hotmail.com>
2024-05-04 01:45:20 -0700

    docs (main): helper functions

--- main.py
+++ main.py
@@ -1,15 +1,35 @@
 import sys
 
 def get_videos():
+    """
+    Gets the videos from a specific directory.
+    Should be looking for videos that have not been added previously.
+
+    :return: list of strings that represent the videos to be uploaded
+    """
     pass
 
-def upload_details():
+def upload_details(videos):
+    """
+    For the given videos we will prompt the user to enter the details needed to be sent to YouTube.
+
+    :param videos: list of strings that represent the videos to be uploaded
+    :return: a list of dictionaries that represent the details needed to be sent to YouTube
+    """
     pass
 
 def upload_videos():
+    """
+    WORK IN PROGRESS...
+    """
     pass
 
 def mark_videos():
+    """
+    Takes the previously uploaded videos and marks them in the directory.
+
+    :param videos: list of strings that represent the videos to be uploaded
+    """
     pass
 
 def main():
