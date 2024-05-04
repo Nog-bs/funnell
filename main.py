b1e341c20a7c809358f673b4a384aeca0f90eb5c
Edro Gonzales <edrogonzales@hotmail.com>
2024-05-04 01:10:58 -0700

    add (main): create flow

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import sys
+
+def get_videos():
+    pass
+
+def upload_details():
+    pass
+
+def upload_videos():
+    pass
+
+def mark_videos():
+    pass
+
 def main():
-    print("Hello World!")
+    """ Drives the program """
+    videos_path = get_videos()
+    videos_to_upload = upload_details(videos_path)
+    try:
+        upload_videos(videos_to_upload)
+    except:
+        print("Seems like there was an issue: {}", file=sys.stderr)
+        return
+    mark_videos(videos_to_upload)
+    print("Videos uploaded to Youtube, keep on dancing!")
 
 
 if __name__ == '__main__':
